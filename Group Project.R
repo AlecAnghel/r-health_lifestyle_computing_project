8f297fa23a8fb029f697ea5f1e1e81f3a88ed728
Alec Anghel <alec.anghel@emory.edu>
2025-11-05 10:24:52 -0500

    Environment Setup

--- Group Project.R
+++ Group Project.R
@@ -1,4 +1,12 @@
-head(life_style)
+library(janitor)
+sleep <- clean_names(sleep)
+lifestyle <- clean_names(lifestyle)
 
-print("testing git")
-print("testing again")
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+
+ggplot(sleep, aes(x = physical_activity_level, y = quality_of_sleep))+
+  geom_point()+
+  geom_smooth()
