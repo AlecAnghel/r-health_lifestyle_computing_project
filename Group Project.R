01a6580da93eebda371c01b239518b8452e4f1ca
Alec Anghel <alec.anghel@emory.edu>
2025-11-17 19:48:07 -0500

    more EDA

--- Group Project.R
+++ Group Project.R
@@ -1,7 +1,7 @@
 library(janitor)
-student_performance <- clean_names(student_performance)
-performance_fac <- clean_names(performance_fac)
-exam_scores <- clean_names(exam_scores)
+AcademicPerformance <- clean_names(StudentPerformance)
+StudentPerformanceFactors <- clean_names(StudentPerformanceFactors)
+ExamScores <- clean_names(ExamScores)
 
 library(ggplot2)
 library(dplyr)
@@ -9,17 +9,17 @@ library(tidyr)
 library(tidyverse)
 library(plotly)
 
-ggplot(performance_fac, aes(x = teacher_quality, y = exam_score))+
+ggplot(StudentPerformanceFactors, aes(x = teacher_quality, y = exam_score))+
   geom_boxplot()
 
 # Comparing categorical variables (comparing family income and school type)
-p <- ggplot(performance_fac, aes(x = family_income, fill = school_type)) +
+p <- ggplot(StudentPerformanceFactors, aes(x = family_income, fill = school_type)) +
   geom_bar(position = "dodge") +
   labs(title = "Family Income and School Type",
        x = "Family Income",
        y = "Count") +
   theme_minimal() +
-  scale_fill_manual(values = c("lightblue", "pink"))
+  scale_fill_manual(name = "School Type",values = c("lightblue", "pink"))
 
 # Make the plot interactive
 p_interactive_FamilyIncome_SchoolType <- ggplotly(p)
@@ -27,20 +27,20 @@ p_interactive_FamilyIncome_SchoolType
 
 # Comparing categorical variables (comparing school type and teacher level)
 # School type vs teacher quality plot
-p2<-ggplot(performance_fac, aes(x = school_type, fill = teacher_quality)) +
+p2<-ggplot(StudentPerformanceFactors, aes(x = school_type, fill = teacher_quality)) +
   geom_bar(position = "dodge") +
   labs(title = "School Type and Teacher Quality",
        x = "School Type",
        y = "Count") +
   theme_minimal() +
-  scale_fill_manual(values = c("lightblue", "pink", "forestgreen"))
+  scale_fill_manual(name = "Teacher Quality", values = c("lightblue", "pink", "forestgreen"))
 
 # Make the plot interactive
 p_interactive_SchoolType_TeacherQuality <- ggplotly(p2)
 p_interactive_SchoolType_TeacherQuality
 
 # Percentage of high, medium, and low-income families sending kids to private vs public schools
-income_school_breakdown <- performance_fac %>%
+income_school_breakdown <- StudentPerformanceFactors %>%
   group_by(family_income, school_type) %>%
   summarise(
     count = n()
@@ -56,7 +56,7 @@ income_school_breakdown <- performance_fac %>%
 print(income_school_breakdown)
 
 # Percentage of teachers with high, medium, or low quality in private vs public schools
-teacher_quality_school_breakdown <- performance_fac %>%
+teacher_quality_school_breakdown <- StudentPerformanceFactors %>%
   group_by(school_type, teacher_quality) %>%
   summarise(
     count = n()
@@ -70,3 +70,32 @@ teacher_quality_school_breakdown <- performance_fac %>%
 
 # Print result for teacher quality by school type
 print(teacher_quality_school_breakdown)
+
+
+# Sleep hours and exam scores
+p3 <- ggplot(ExamScores, aes(sleep_hours, exam_score))+
+  geom_point(color = "blue",shape = "diamond", alpha = 0.7)+
+  geom_smooth(color = "purple",method = "lm")+
+  labs(
+    title = "Sleep Hours vs Exam Scores",
+    y = "Exam Scores",
+    x = "Sleep Hours")+
+  theme_minimal()
+
+#NOT MUCH OF A RELATIONSHIP
+p_interactive_SleepHours_ExamScores <- ggplotly(p3)
+p_interactive_SleepHours_ExamScores
+
+# Hours studied and attendance percent
+p4 <- ggplot(ExamScores, aes(hours_studied,attendance_percent))+
+  geom_point(color = "forestgreen", shape = "square", alpha = 0.7)+
+  geom_smooth(color = "cyan", method = "lm")+
+  labs(
+    title = "Hours Studied vs Attendance Percent",
+    y = "Attendence Percent",
+    x = "Hours Studied") +
+  theme_light()
+
+# NOT MUCH OF A RELATIONSHIP
+p_interactive_HoursStudied_AttendancePercent <- ggplotly(p4)
+p_interactive_HoursStudied_AttendancePercent
